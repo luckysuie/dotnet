e65e1ce4cb5d900d04b2dd11728afb0d8d41c933
luckyashu1856 <luckyashu1856@gmail.com>
2025-06-30 01:57:28 +0000

    Added main.tf

---
+++ infra/main.tf
@@ -0,0 +1,28 @@
+Terraform app service deployment code: 
+provider "azurerm" { 
+features {} 
+subscription_id = "946461f2-5424-4818-bd06-010e5f3cd8c1" 
+} 
+resource "azurerm_resource_group" "rg" { 
+name     = "rg-dotnet-app" 
+location = "Canada Central" 
+} 
+resource "azurerm_service_plan" "asp" { 
+name                = "dotnet-app-plan" 
+location            = azurerm_resource_group.rg.location 
+resource_group_name = azurerm_resource_group.rg.name 
+sku_name = "F1"  # Free tier 
+os_type  = "Windows" 
+} 
+resource "azurerm_windows_web_app" "app" { 
+name                = "DotNetWebApp22" 
+location            = azurerm_resource_group.rg.location 
+resource_group_name = azurerm_resource_group.rg.name 
+service_plan_id     = azurerm_service_plan.asp.id 
+site_config { 
+always_on = true 
+} 
+tags = { 
+environment = "dev" 
+} 
+}
