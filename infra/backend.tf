f58a68cd0bc64f33b506b42ec3bca9f31ae6ce29
luckyashu1856 <luckyashu1856@gmail.com>
2025-07-02 12:37:26 +0000

    Added backend.tf

---
+++ infra/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "lucky"
+    storage_account_name = "luckystorage1234"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}  
